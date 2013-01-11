453e4f27e5a875cc315aaab4336f3d7aff1ba83e
Steven Morell <sven@3xperts.com>
2013-01-11 18:43:32 -0500

    email changes

---
+++ application/views/front/mail.blade.php
@@ -0,0 +1,9 @@
+<p>Vorname: {{ $data['first_name'] }} </p>
+<p>Nachname: {{ $data['last_name'] }}</p>
+<p>Email: {{ $data['email'] }}</p>
+<p>Situation: {{ $data['situation'] }}</p>
+<p>Anrede: {{ $data['salutation'] }}</p>
+<p>Fon: {{ $data['phone'] }}</p>
+<p>Strasse: {{ $data['street'] }}</p>
+<p>PLZ: {{ $data['zipcode'] }}</p>
+<p>Ort: {{ $data['city'] }}</p>
